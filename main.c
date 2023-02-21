201b0d41a0aa3db765702e15f084666d2883addf
Stefan <stefan.holmberg@systementor.se>
2023-02-21 08:01:51 +0100

    join files

--- main.c
+++ main.c
@@ -21,8 +21,42 @@ void oddRows(){
 }
 
 
+void joinFiles(){
+    FILE *fResult = fopen("resultat.txt", "w");
+    FILE *f1=fopen("namn.txt", "r");
+    FILE *f2=fopen("namn2.txt", "r");
+    if(f1 == NULL  || f2 == NULL || fResult == NULL){
+        printf("FEL!!! - Filen kunde inte öppnas");
+        fclose(f1);
+        fclose(f2);
+        fclose(fResult);
+        return;
+   }
+    
+    char line[250];
+    while(fgets(line,250,f1)){
+        fputs(line,fResult);
+    }
+    fclose(f1);
+
+    while(fgets(line,250,f2)){
+        fputs(line,fResult);
+    }
+    fclose(f2);
+
+    fclose(fResult);
+
+
+
+    
+}
+
+
+
+
 void main(){
-    oddRows();
+    joinFiles();
+    //oddRows();
 }
 
 
